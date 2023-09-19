24fd46624cdc7490e82b23906b064f9a9346e128
Henil Malaviya <henilmalaviya06@gmail.com>
2023-09-19 15:03:43 +0530

    init

---
+++ src/lib/info.js
@@ -0,0 +1,24 @@
+// This is for convenience as you start out with this template.
+// If you'd rather hardcode these values into your pages,
+// feel free to remove this file.
+
+// URL of your website
+export const website = 'https://blog.henil.xyz'
+
+export const firstName = 'Henil'
+export const lastName = 'Malaviya'
+export const name = `${firstName} ${lastName}`
+
+// Used for the landing page & footer of posts
+export const avatar = '/henil.jpg'
+export const bio = `\
+I’m ${firstName}, an under graduate full stack web developer based in India. I love to write about different challenges i face during development of projects.
+`
+
+// Your social usernames, empty strings won't be used.
+// For sake of the live demo, I'm using `username/repo` for github,
+// but you can replace with just your username.
+export const github = 'henil0604'
+export const twitter = 'realhenil'
+export const linkedin = 'henil-malaviya'
+export const instagram = 'henilmalaviya'
